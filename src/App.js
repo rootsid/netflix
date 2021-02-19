d767747bacef08f47fa5e8cecd7aa5eac89a2600
sidharth kaushik <sidharth.kaushik@atos.net>
2021-02-19 14:19:12 +0530

    requests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Sid! Let's build the react app.</h1>
+      <Row title="Netflix ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
